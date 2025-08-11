4f4e71cee68cc6ed3f253616e921b766fabc91d6
Tafadzwa Ben <tbmukwepa@gmail.com>
2025-08-11 16:55:06 +0200

    created logs/index.blade.php -changed the config/app.php time to Africa/Harare

---
+++ resources/views/logs/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Logs</h1>
+        <a href="{{ route('logs.create') }}" class="btn btn-primary mb-3">Add Log</a>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>Time</th>
+                <th>Type</th>
+                <th>Title</th>
+                <th>System</th>
+                <th>User</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($logs as $log)
+                <tr>
+                    <td>{{ $log->event_time }}</td>
+                    <td>{{ $log->type->name }}</td>
+                    <td>{{ $log->title }}</td>
+                    <td>{{ $log->affected_system }}</td>
+                    <td>{{ $log->user->name }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        {{ $logs->links() }}
+    </div>
+@endsection
